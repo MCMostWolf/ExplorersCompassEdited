0f0f7651191a5f5cf052ab23bd43f4b89efbebe4
MC作死狼王 <81859193+MCMostWolf@users.noreply.github.com>
2025-11-02 05:53:46 +0800

    Add files via upload

--- src/main/java/com/chaosthedude/explorerscompass/ExplorersCompass.java
+++ src/main/java/com/chaosthedude/explorerscompass/ExplorersCompass.java
@@ -1,22 +1,18 @@
 package com.chaosthedude.explorerscompass;
 
-import java.io.IOException;
 import java.util.ArrayList;
 import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
 
 import com.chaosthedude.explorerscompass.config.BetterUI;
-import com.chaosthedude.explorerscompass.network.CleanFoundPacket;
+import com.chaosthedude.explorerscompass.network.*;
 import org.apache.logging.log4j.LogManager;
 import org.apache.logging.log4j.Logger;
 
 import com.chaosthedude.explorerscompass.client.ClientEventHandler;
 import com.chaosthedude.explorerscompass.config.ConfigHandler;
 import com.chaosthedude.explorerscompass.items.ExplorersCompassItem;
-import com.chaosthedude.explorerscompass.network.CompassSearchPacket;
-import com.chaosthedude.explorerscompass.network.SyncPacket;
-import com.chaosthedude.explorerscompass.network.TeleportPacket;
 import com.chaosthedude.explorerscompass.util.CompassState;
 import com.google.common.collect.ArrayListMultimap;
 import com.google.common.collect.ListMultimap;
@@ -46,6 +42,7 @@ import net.minecraftforge.fml.event.lifecycle.FMLCommonSetupEvent;
 import net.minecraftforge.fml.javafmlmod.FMLJavaModLoadingContext;
 import net.minecraftforge.network.NetworkRegistry;
 import net.minecraftforge.network.simple.SimpleChannel;
+import org.jetbrains.annotations.NotNull;
 
 @Mod(ExplorersCompass.MODID)
 public class ExplorersCompass {
@@ -67,9 +64,7 @@ public class ExplorersCompass {
 		FMLJavaModLoadingContext.get().getModEventBus().addListener(this::commonSetup);
 		FMLJavaModLoadingContext.get().getModEventBus().addListener(this::buildCreativeTabContents);
 		
-		DistExecutor.unsafeRunWhenOn(Dist.CLIENT, () -> () -> {
-			FMLJavaModLoadingContext.get().getModEventBus().addListener(this::clientSetup);
-		});
+		DistExecutor.unsafeRunWhenOn(Dist.CLIENT, () -> () -> FMLJavaModLoadingContext.get().getModEventBus().addListener(this::clientSetup));
 		
 		ModLoadingContext.get().registerConfig(ModConfig.Type.COMMON, ConfigHandler.GENERAL_SPEC);
 		ModLoadingContext.get().registerConfig(ModConfig.Type.CLIENT, ConfigHandler.CLIENT_SPEC);
@@ -78,21 +73,26 @@ public class ExplorersCompass {
 
 	private void commonSetup(FMLCommonSetupEvent event) {
 		network = NetworkRegistry.newSimpleChannel(new ResourceLocation(MODID, MODID), () -> "1.0", s -> true, s -> true);
+        int i=0;
 
 		// Server packets
-		network.registerMessage(0, CompassSearchPacket.class, CompassSearchPacket::toBytes, CompassSearchPacket::new, CompassSearchPacket::handle);
-		network.registerMessage(1, TeleportPacket.class, TeleportPacket::toBytes, TeleportPacket::new, TeleportPacket::handle);
+		network.registerMessage(i, CompassSearchPacket.class, CompassSearchPacket::toBytes, CompassSearchPacket::new, CompassSearchPacket::handle);
+        i++;
+		network.registerMessage(i, TeleportPacket.class, TeleportPacket::toBytes, TeleportPacket::new, TeleportPacket::handle);
+        i++;
+        network.registerMessage(i, NewWayPointPacket.class, NewWayPointPacket::toBytes, NewWayPointPacket::new, NewWayPointPacket::handle);
+
 
 		// Client packet
-		network.registerMessage(2, SyncPacket.class, SyncPacket::toBytes, SyncPacket::new, SyncPacket::handle);
+        i++;
+		network.registerMessage(i, SyncPacket.class, SyncPacket::toBytes, SyncPacket::new, SyncPacket::handle);
+        i++;
+		network.registerMessage(i, CleanFoundPacket.class, CleanFoundPacket::toBytes, CleanFoundPacket::new, CleanFoundPacket::handle);
 
-		network.registerMessage(3, CleanFoundPacket.class, CleanFoundPacket::toBytes, CleanFoundPacket::new, CleanFoundPacket::handle);
-
-		allowedStructureKeys = new ArrayList<ResourceLocation>();
+		allowedStructureKeys = new ArrayList<>();
 		dimensionKeysForAllowedStructureKeys = ArrayListMultimap.create();
-		structureKeysToTypeKeys = new HashMap<ResourceLocation, ResourceLocation>();
+		structureKeysToTypeKeys = new HashMap<>();
 		typeKeysToStructureKeys = ArrayListMultimap.create();
-
 		BetterUI.loadConfig(MODID);
 	}
 	
@@ -106,78 +106,78 @@ public class ExplorersCompass {
 	public void clientSetup(FMLClientSetupEvent event) {
 		MinecraftForge.EVENT_BUS.register(new ClientEventHandler());
 		
-		event.enqueueWork(() -> {
-			ItemProperties.register(explorersCompass, new ResourceLocation("angle"), new ClampedItemPropertyFunction() {
-				@OnlyIn(Dist.CLIENT)
-				private double rotation;
-				@OnlyIn(Dist.CLIENT)
-				private double rota;
-				@OnlyIn(Dist.CLIENT)
-				private long lastUpdateTick;
-	
-				@OnlyIn(Dist.CLIENT)
-				@Override
-				public float unclampedCall(ItemStack stack, ClientLevel world, LivingEntity entityLiving, int seed) {
-					if (entityLiving == null && !stack.isFramed()) {
-						return 0.0F;
-					} else {
-						final boolean entityExists = entityLiving != null;
-						final Entity entity = (Entity) (entityExists ? entityLiving : stack.getFrame());
-						if (world == null && entity.level() instanceof ClientLevel) {
-							world = (ClientLevel) entity.level();
-						}
-	
-						double rotation = entityExists ? (double) entity.getYRot() : getFrameRotation((ItemFrame) entity);
-						rotation = rotation % 360.0D;
-						double adjusted = Math.PI - ((rotation - 90.0D) * 0.01745329238474369D - getAngle(world, entity, stack));
-	
-						if (entityExists) {
-							adjusted = wobble(world, adjusted);
-						}
-	
-						final float f = (float) (adjusted / (Math.PI * 2D));
-						return Mth.positiveModulo(f, 1.0F);
-					}
-				}
-	
-				@OnlyIn(Dist.CLIENT)
-				private double wobble(ClientLevel world, double amount) {
-					if (world.getGameTime() != lastUpdateTick) {
-						lastUpdateTick = world.getGameTime();
-						double d0 = amount - rotation;
-						d0 = Mth.positiveModulo(d0 + Math.PI, Math.PI * 2D) - Math.PI;
-						d0 = Mth.clamp(d0, -1.0D, 1.0D);
-						rota += d0 * 0.1D;
-						rota *= 0.8D;
-						rotation += rota;
-					}
-	
-					return rotation;
-				}
-	
-				@OnlyIn(Dist.CLIENT)
-				private double getFrameRotation(ItemFrame itemFrame) {
-					Direction direction = itemFrame.getDirection();
-					int i = direction.getAxis().isVertical() ? 90 * direction.getAxisDirection().getStep() : 0;
-					return (double)Mth.wrapDegrees(180 + direction.get2DDataValue() * 90 + itemFrame.getRotation() * 45 + i);
-				}
-	
-				@OnlyIn(Dist.CLIENT)
-				private double getAngle(ClientLevel world, Entity entity, ItemStack stack) {
-					if (stack.getItem() == explorersCompass) {
-						ExplorersCompassItem compassItem = (ExplorersCompassItem) stack.getItem();
-						BlockPos pos;
-						if (compassItem.getState(stack) == CompassState.FOUND) {
-							pos = new BlockPos(compassItem.getFoundStructureX(stack), 0, compassItem.getFoundStructureZ(stack));
-						} else {
-							pos = world.getSharedSpawnPos();
-						}
-						return Math.atan2((double) pos.getZ() - entity.position().z(), (double) pos.getX() - entity.position().x());
-					}
-					return 0.0D;
-				}
-			});
-		});
+		event.enqueueWork(() -> ItemProperties.register(explorersCompass, new ResourceLocation("angle"), new ClampedItemPropertyFunction() {
+            @OnlyIn(Dist.CLIENT)
+            private double rotation;
+            @OnlyIn(Dist.CLIENT)
+            private double rota;
+            @OnlyIn(Dist.CLIENT)
+            private long lastUpdateTick;
+
+            @OnlyIn(Dist.CLIENT)
+            @Override
+            public float unclampedCall(@NotNull ItemStack stack, ClientLevel world, LivingEntity entityLiving, int seed) {
+                if (entityLiving == null && !stack.isFramed()) {
+                    return 0.0F;
+                } else {
+                    final boolean entityExists = entityLiving != null;
+                    final Entity entity = entityExists ? entityLiving : stack.getFrame();
+                    if (entity != null && world == null && entity.level() instanceof ClientLevel) {
+                        world = (ClientLevel) entity.level();
+                    }
+
+                    double rotation = entityExists ? (double) entity.getYRot() : getFrameRotation((ItemFrame) entity);
+                    rotation = rotation % 360.0D;
+                    double adjusted = Math.PI - ((rotation - 90.0D) * 0.01745329238474369D - getAngle(world, entity, stack));
+
+                    if (entityExists) {
+                        if (world != null) {
+                            adjusted = wobble(world, adjusted);
+                        }
+                    }
+
+                    final float f = (float) (adjusted / (Math.PI * 2D));
+                    return Mth.positiveModulo(f, 1.0F);
+                }
+            }
+
+            @OnlyIn(Dist.CLIENT)
+            private double wobble(ClientLevel world, double amount) {
+                if (world.getGameTime() != lastUpdateTick) {
+                    lastUpdateTick = world.getGameTime();
+                    double d0 = amount - rotation;
+                    d0 = Mth.positiveModulo(d0 + Math.PI, Math.PI * 2D) - Math.PI;
+                    d0 = Mth.clamp(d0, -1.0D, 1.0D);
+                    rota += d0 * 0.1D;
+                    rota *= 0.8D;
+                    rotation += rota;
+                }
+
+                return rotation;
+            }
+
+            @OnlyIn(Dist.CLIENT)
+            private double getFrameRotation(ItemFrame itemFrame) {
+                Direction direction = itemFrame.getDirection();
+                int i = direction.getAxis().isVertical() ? 90 * direction.getAxisDirection().getStep() : 0;
+                return Mth.wrapDegrees(180 + direction.get2DDataValue() * 90 + itemFrame.getRotation() * 45 + i);
+            }
+
+            @OnlyIn(Dist.CLIENT)
+            private double getAngle(ClientLevel world, Entity entity, ItemStack stack) {
+                if (stack.getItem() == explorersCompass) {
+                    ExplorersCompassItem compassItem = (ExplorersCompassItem) stack.getItem();
+                    BlockPos pos;
+                    if (compassItem.getState(stack) == CompassState.FOUND) {
+                        pos = new BlockPos(compassItem.getFoundStructureX(stack), 0, compassItem.getFoundStructureZ(stack));
+                    } else {
+                        pos = world.getSharedSpawnPos();
+                    }
+                    return Math.atan2((double) pos.getZ() - entity.position().z(), (double) pos.getX() - entity.position().x());
+                }
+                return 0.0D;
+            }
+        }));
 	}
 
 }
